2996144e8ab1caef73a824469e1b48e4a84478b6
COOLKIDZYS <1969702500@qq.com>
2020-03-20 13:09:54 +0800

    The second commit of sparkstreaming

--- src/main/scala/cn/sheep/utils/JedisUtils.scala
+++ src/main/scala/cn/sheep/utils/JedisUtils.scala
@@ -4,7 +4,7 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig
 import redis.clients.jedis.{Jedis, JedisPool}
 
 object JedisUtils {
-  val jedisPool = new JedisPool(new GenericObjectPoolConfig(), "node-1")
+  val jedisPool = new JedisPool(new GenericObjectPoolConfig(), "coolkid")
   def getJedisClient():Jedis=
     jedisPool.getResource
 
